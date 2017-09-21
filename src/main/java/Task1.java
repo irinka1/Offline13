0fdd9c02a0242932ce3fc335c2a3be9cb6cdfb3c
Irinka <irenmast@gmail.com>
2017-09-21 22:11:04 +0300

    3

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -47,19 +47,18 @@ public class Task1 {
         Scanner sc = new Scanner(System.in);
         // int a = sc.nextInt();
 
-        List<String> videoID = new ArrayList<String>();
-        for (int i6 = 0; i6 < 10; i6++) {
-            videoID.add(sc.nextLine());
-        }
-            System.out.println(videoID);
 
-            initApplication();
+        String[] videoID = sc.nextLine().split(" ");
 
 
+        initApplication();
+
+
+        for (int i6 = 0; i6 < videoID.length; i6++) {
 
             HttpResponse<ActivityResponse> response = Unirest.get("https://www.googleapis.com/youtube/v3/commentThreads")
                     .queryString("part", "snippet")
-                    .queryString("videoId", "l_yJ0aH1tqE")
+                    .queryString("videoId", videoID[i6])
                     // .queryString("maxResults", maxResults)
                     .queryString("key", "AIzaSyAoJtBYFKQT3rF5Qy0P0hHEFdAMDnOXOhA")
                     .asObject(ActivityResponse.class);
@@ -67,18 +66,17 @@ public class Task1 {
             // System.out.println(response.getBody());
             // System.out.println(response.getStatus());
 
+
             ActivityResponse activity = response.getBody();
 
             for (int i = 0; i < activity.items.size(); i++) {
                 final Activity item = activity.items.get(i);
                 String authorDisplayName = item.snippet.topLevelComment.snippet.authorDisplayName;
-               // System.out.println(authorDisplayName);
+                 System.out.println(authorDisplayName);
             }
 
-        for (int i6 = 0; i6 < 10; i6++) {
-           // videoID.get(i6) =
-        }
 
         }
     }
+}
 
